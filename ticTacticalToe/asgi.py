f7fe1d7544559439315e26cda630e9849da62ab3
Cory Anderson <corya14@gmail.com>
2021-04-19 13:51:42 -0400

    Fix asgi settings

--- ticTacticalToe/asgi.py
+++ ticTacticalToe/asgi.py
@@ -15,21 +15,10 @@ from django.core.asgi import get_asgi_application
 import ticTacticalToe.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticTacticalToe.settings')
-
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            ticTacticalToe.routing.websocket_urlpatterns
-        )
-    ),
-})
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
         URLRouter(
             ticTacticalToe.routing.websocket_urlpatterns
         )
