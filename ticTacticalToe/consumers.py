3990d0a1ce3325074a459e0e63cbf077118a2a00
Cory Anderson <corya14@gmail.com>
2021-04-19 17:42:05 -0400

    Websocket groups

--- ticTacticalToe/consumers.py
+++ ticTacticalToe/consumers.py
@@ -1,17 +1,21 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from game.models import MockGame
+from asgiref.sync import async_to_sync
 #from channels.generic.websocket import JsonWebsocketConsumer
 #from channels.generic.websocket import AsyncWebsocketConsumer
 
 class UserSocketConsumer(WebsocketConsumer):
     def connect(self):
         self.user = self.scope["user"]
-        self.accept()
-        self.send( json.dumps( MockGame.gen_initial_game_dict() ) )
+        if self.user.is_authenticated:
+            async_to_sync(self.channel_layer.group_add)(self.user.username, self.channel_name)
+            self.accept()
+            self.send( json.dumps( MockGame.gen_initial_game_dict() ) )
 
     def disconnect(self, close_code):
-        pass
+        if self.user.is_authenticated:
+            async_to_sync(self.channel_layer.group_discard)(self.user.username, self.channel_name)
 
     def receive(self, text_data):
         if self.user.is_authenticated:
@@ -21,8 +25,13 @@ class UserSocketConsumer(WebsocketConsumer):
 
             game_dict = MockGame.gen_dummy_dict()
 
-            self.send( text_data=json.dumps( game_dict ) )
+            async_to_sync(self.channel_layer.group_send)(
+                self.user.username,
+                {
+                    "type": "gameboard_update",
+                    "text": json.dumps( game_dict ),
+                },
+            )
 
-            # self.send(text_data=json.dumps({
-            #     'message': message
-            # }))
+    def gameboard_update(self, event):
+        self.send( text_data=event["text"] )
