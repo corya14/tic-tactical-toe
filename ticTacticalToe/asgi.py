b6e3ff5711e7c575fe69f6367866a3f70ea2dd7b
Cory Anderson <corya14@gmail.com>
2021-04-19 14:31:12 -0400

    Fix asgi.py

--- ticTacticalToe/asgi.py
+++ ticTacticalToe/asgi.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import ticTacticalToe.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticTacticalToe.settings')
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticTacticalToe.settings')
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
