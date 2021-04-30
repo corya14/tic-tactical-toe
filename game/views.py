479292ca1a70758d85c9e74a9649dc01a57b41cb
Cory Anderson <corya14@gmail.com>
2021-04-30 12:05:33 -0400

    closes #2 closes #3; Regex verification of game names

--- game/views.py
+++ game/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from game.interfaces import GameModelInterface
 from django.contrib import messages
+import re
+import logging
+
+authlog = logging.getLogger('auth')
+
+VERIFY_REGEX = re.compile(r'^[a-zA-Z0-9_]+$')
 
 # Create your views here.
 
@@ -14,11 +20,19 @@ def lobby(request):
 
 def gameroom(request, game_name):
     if request.user.is_authenticated:
-        if GameModelInterface.user_is_authenticated_to_game(request.user, game_name):
-            return render(request, 'game/gameroom.html', {
-                'game_name': game_name,
-                'requesting_user': request.user.username
-            })
+        if VERIFY_REGEX.match(game_name):
+            if GameModelInterface.user_is_authenticated_to_game(request.user, game_name):
+                return render(request, 'game/gameroom.html', {
+                    'game_name': game_name,
+                    'requesting_user': request.user.username
+                })
+            else:
+                authlog.warning('User {} failed to authenticate to game {}'.format(
+                    request.user.username, game_name))
+                messages.error(request, "Invalid game name. Please try again.")
+                return redirect('lobby')
         else:
+            authlog.debug('User {} tried to create a game with an invalid name.'.format(
+                request.user.username))
             messages.error(request, "Invalid game name. Please try again.")
             return redirect('lobby')
